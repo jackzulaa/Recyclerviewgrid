81b19aae8700ccbb88c2aa6076e6e8e9e95a6a0c
moses okoth <jackmokn@gmail.com>
2022-04-03 12:39:33 +0300

    commiting changes to recycler

--- app/src/main/java/com/mokoth/recyclerviewgrid/MainActivity.java
+++ app/src/main/java/com/mokoth/recyclerviewgrid/MainActivity.java
@@ -1,14 +1,37 @@
 package com.mokoth.recyclerviewgrid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+
+    // Using ArrayList to store images data
+    ArrayList images = new ArrayList<>(Arrays.asList(R.drawable.img_1, R.drawable.img_2, R.drawable.img_3,
+            R.drawable.img_4, R.drawable.img_5, R.drawable.img_6, R.drawable.img_7, R.drawable.img_8,
+            R.drawable.img_9, R.drawable.img_10));
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Getting reference of recyclerView
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        // Setting the layout as Staggered Grid for vertical orientation
+        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+
+        // Sending reference and data to Adapter
+        Adapter adapter = new Adapter(MainActivity.this, images);
+
+        // Setting Adapter to RecyclerView
+        recyclerView.setAdapter(adapter);
     }
 }
